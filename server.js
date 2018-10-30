3017a2925eab3c5bbbd5748d7bc8d79f42951054
Aleksandra Samsonovitch <aleksandra@Aleksandras-MacBook-Pro.local>
2018-10-30 17:27:58 +0200

    first

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var app = express();
 //app.configure, app.use etc
 
 
-app.use(express.static('public/index-1.html'));
+app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
+app.get('/', function (req, res) {
+  res.send('This is from the root directory')
+});
 
 app.listen(process.env.PORT || '8000', function(){
   console.log("8000. Baruh Hashem!")
