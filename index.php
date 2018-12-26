5a99b0ce8c0ff9657c6b3bba313d548ee6213e18
akee01 <muhammadaqibrasheed@gmail.com>
2018-12-26 18:21:41 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
     filter: brightness(1.5);
 
 ">
-        <ul class="bubbles">
+        <!-- <ul class="bubbles">
             <li class="bubbles-1"></li>
             <li class="bubbles-2"></li>
             <li class="bubbles-3"></li>
@@ -116,12 +116,10 @@
             <li class="bubbles-5"></li>
             <li class="bubbles-6"></li>
             <li class="bubbles-7"></li>
-        </ul>
+        </ul> -->
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
-            <video id="video-background" autoplay loop autobuffer muted playsinline>
-              <source src="./video/ISRAELACCUEIL.mp4" type="video/mp4">
-            </video>
+
             <!-- Wrapper for slides -->
              <div class="carousel-inner" role="listbox">
 
@@ -135,7 +133,9 @@
 
                 </div>
 
-                          
+                          <video id="video-background" autoplay loop autobuffer muted playsinline>
+  <source src="./video/ISRAELACCUEIL.mp4" type="video/mp4">
+</video>
   </div>
           </div>
 
